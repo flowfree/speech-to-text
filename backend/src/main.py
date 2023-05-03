36cde98730ec9effa60a238f0526dd0d6e4b3767
Nashruddin Amin <nashruddin.amin@gmail.com>
2023-05-03 08:20:43 +0700

    Added docker compose

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 import tempfile
 import pathlib
@@ -32,6 +33,7 @@ def home():
 
 
 model = 'openai/whisper-small.en'
+logging.info(f'Downloading model {model}...')
 transcriber = pipeline(
     'automatic-speech-recognition', 
     model=model,
